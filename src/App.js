85e6df725475e7e1a5f1e775bcc2ba541b839eb9
vishalkanu01 <vishalkanu01@gmail.com>
2022-12-01 21:52:05 +0545

    Dynamic search

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import FilterableProductTable from './Components/FilterableProductTable';
+
+
+const PRODUCTS = [
+  {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
+  {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
+  {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
+  {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
+  {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
+  {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
+];
+function App() {
+  return (
+    <div className="App">
+      <FilterableProductTable products={PRODUCTS} />
+    </div>
+  );
+}
+
+export default App;
